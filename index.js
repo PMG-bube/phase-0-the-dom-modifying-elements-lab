7ecd00e5c17db78819a4de18265159f9259743ed
pmg <gachigipm@gmail.com>
2024-10-13 18:14:40 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const rmv=document.querySelector("#main");
+rmv.remove();
+const newHeader=document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent="DEXTEROUS-DAWATI is the champion"
+
